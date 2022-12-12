046051223e2423578fa5e8edb073c35fff348c96
Nick <nick@inspirationhm.com>
2022-12-12 15:31:01 +0800

    Finalize project

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -4,16 +4,24 @@ import { useParams, Link } from "react-router-dom";
 import { FaCodepen, FaStore, FaUserFriends, FaUsers } from "react-icons/fa";
 import Spinner from "../components/layout/spinners";
 import RepoList from "../components/repos/RepoList";
+import { getUserAndRepos} from "../context/github/GitActions"
 
 function User() {
-  const { getUser, user, loading, getUserRepos, repos } = useContext(GithubContext);
+  const { user, loading, repos, dispatch } = useContext(GithubContext);
 
   const params = useParams();
 
   useEffect(() => {
-    getUser(params.login);
-    getUserRepos(params.login);
-  }, []);
+    dispatch({type: 'SET_LOADING'})
+    const getUserData = async()=> {
+      const userData = await getUserAndRepos(params.login)
+      dispatch({type: 'GET_USER_AND_REPOS', payload: userData})
+
+    }
+  
+
+  getUserData()}
+  , [ dispatch, params.login])
 
   const {
     name,
